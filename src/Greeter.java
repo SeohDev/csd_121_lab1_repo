d781041a583b3af861cb9156e9523fc571b96727
Liam Deschamps <liamdesh1@gmail.com>
2023-03-28 09:03:03 -0400

    push

--- src/Greeter.java
+++ src/Greeter.java
@@ -11,6 +11,7 @@ public class Greeter {
 
     // TODO: return the name entered by the user
     // QUESTION: Why can this method be marked as private?
+    // ANSWER: The method can be marked private because its only used in the greeter class.
     private static void getNameFromGui() {
         String name = JOptionPane.showInputDialog("What is your name?");
     }
@@ -20,10 +21,18 @@ public class Greeter {
      * Prints the message "Hello, {name}!" to the console
      * @param name the name of the person to greet
      */
+    public static void printName(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
 
     public static void main(String[] args) {
         // TODO: call the getNameFromGui method and store the result in a variable
+        // We need to modify the `getNameFromGui` method to return the name entered by the user
+        // and assign it to a variable in the `main` method
+        String name = JOptionPane.showInputDialog("What is your name?");
 
         // TODO: call the printName function with the variable you created above as an argument
+        // We can now call the `printName` method with the name variable
+        printName(name);
     }
 }
